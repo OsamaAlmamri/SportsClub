e50c15b1f120a0f4cae901fd86e681132569d7c1
Osama Al-mamari <yemencoder@gmail.com>
2023-05-30 23:22:47 +0300

    add services

--- Program.cs
+++ Program.cs
@@ -1,58 +1,82 @@
-﻿using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.DependencyInjection;
-
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using System.IO;
-using System.Reflection;
+﻿using Microsoft.EntityFrameworkCore;
 using SportsClub.Models;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using CoreLib.JWTServices.Authenticators;
+using CoreLib.JWTServices.RefreshTokenRepositories;
+using CoreLib.JWTServices.TokenGenerators;
+using CoreLib.JWTServices.TokenValidators;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using CoreLib.Pagination.Services;
+using System.Text;
+var builder = WebApplication.CreateBuilder(args);
 
-namespace SportsClub
+// Add services to the container.
+
+builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<SportsClubContext>(options =>
 {
-    public class Program
+    options.UseSqlServer(builder.Configuration.GetConnectionString("SqlCon2022"));
+});
+
+
+
+//SecretClient keyVaultClient = new SecretClient(
+//    new Uri(_configuration.GetValue<string>("KeyVaultUri")),
+//    new DefaultAzureCredential());
+//authenticationConfiguration.AccessTokenSecret = keyVaultClient.GetSecret("access-token-secret").Value.Value;
+
+
+
+AuthenticationConfiguration authenticationConfiguration = new AuthenticationConfiguration();
+
+builder.Services.AddSingleton(authenticationConfiguration);
+builder.Services.AddSingleton<AccessTokenGenerator>();
+builder.Services.AddSingleton<RefreshTokenGenerator>();
+builder.Services.AddSingleton<RefreshTokenValidator>();
+builder.Services.AddScoped<Authenticator>();
+builder.Services.AddSingleton<TokenGenerator>();
+builder.Services.AddScoped<IRefreshTokenRepository, DatabaseRefreshTokenRepository>();
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(o =>
+{
+    o.TokenValidationParameters = new TokenValidationParameters()
     {
-
-        public static void Main(string[] args)
-        {
-
-            var WebHost = CreateHostBuilder(args).Build();
-
-            RunMigration(WebHost);
-            WebHost.Run();
-        }
-
-        private static void RunMigration(IHost webHost)
-        {
-            using (var scope = webHost.Services.CreateScope())
-            {
-
-                var db = scope.ServiceProvider.GetRequiredService<SportsClubContext>();
-                db.Database.Migrate();
-
-            }
-        }
-
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-
-                    webBuilder.UseContentRoot(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location));
-                });
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(authenticationConfiguration.AccessTokenSecret)),
+        ValidIssuer = authenticationConfiguration.Issuer,
+        ValidAudience = authenticationConfiguration.Audience,
+        ValidateIssuerSigningKey = true,
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ClockSkew = TimeSpan.Zero
+    };
+});
 
 
+var app = builder.Build();
 
-
-
-
-
-
-    }
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
 }
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+app.UseRouting();
+
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller}/{action=Index}/{id?}");
+
+app.MapFallbackToFile("index.html"); ;
+
+app.Run();
